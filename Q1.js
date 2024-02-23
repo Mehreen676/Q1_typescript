0313566533b99df084b5195cd4c12729d1cf473e
Mehreen676 <159542833+Mehreen676@users.noreply.github.com>
2024-02-23 20:55:12 +0500

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,4 @@
+"use strict";
+// Install Node.js, TypeScript and VS Code on your computer.
+// install Node js 
+("https://nodejs.org/")("node -v")("npm -v")("npm install -g typescript")("https://code.visualstudio.com/");
---
+++ Q1.ts
@@ -0,0 +1,15 @@
+// Install Node.js, TypeScript and VS Code on your computer.
+
+// install Node js 
+("https://nodejs.org/")
+
+("node -v")
+("npm -v")
+
+// TypeScript
+("npm install -g typescript")
+
+// Visual Studio Code:
+("https://code.visualstudio.com/")
+
+
